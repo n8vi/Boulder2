906a80eab86c6a4ee4f6198de3b212b8f3370895
Brad Tarratt <n8vi@rfc791.org>
2014-11-14 22:03:44 -0500

    Automatic timezones, finally! (US only, so far)
    Sorry, everyone not in the continental US, but I've only dealt with US time zone display for now.  I may fix this, or do several versions, later, but for now, this is the watchface I wanted.  Yay!

--- src/main.c
+++ src/main.c
@@ -1,11 +1,10 @@
 #include <pebble.h>
 
-#define TZ (-5)
 #define SECONDS
 
-// #define MY_UUID { 0xff, 0x40, 0xe8, 0xb0, 0x52, 0x99, 0x41, 0x4a, 0xa9, 0x3f, 0xcc, 0xc9, 0x09, 0x1d, 0x85, 0x5f }
-// PBL_APP_INFO(MY_UUID, "Boulder", "N8VI", 1, 0 /* App version */, RESOURCE_ID_IMAGE_MENU_ICON, APP_INFO_WATCH_FACE);
-
+static int offset = 0;
+static char tzname[] = "UTC";
+	
 static char *dows[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 static char *mons[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
@@ -17,6 +16,11 @@ TextLayer *text_UTC_layer;
 TextLayer *text_batt_layer;
 TextLayer *text_conn_layer;
 TextLayer *text_dst_layer;
+
+bool isDst(void) /* sucks how the API doesn't work */
+{                /* need to find workaround */
+	  return 0;
+}
 
 void handle_conn(bool connected)
 {
@@ -36,21 +40,6 @@ void handle_batt(BatteryChargeState charge)
   text_layer_set_text(text_batt_layer, battstate);
 }
 
-// Layer line_layer;
-/*
-void line_layer_update_callback(Layer *me, GContext* ctx) 
-{
-  (void)me;
-
-  graphics_context_set_stroke_color(ctx, GColorWhite);
-
-  graphics_draw_line(ctx, GPoint(8, 51), GPoint(131, 51));
-  graphics_draw_line(ctx, GPoint(8, 52), GPoint(131, 52));
-  graphics_draw_line(ctx, GPoint(8, 115), GPoint(131, 115));
-  graphics_draw_line(ctx, GPoint(8, 116), GPoint(131, 116));
-
-}
-*/
 int julian( int unixSecs )
 {
    return (int)(( unixSecs / 86400.0 ) + 2440587.5);
@@ -127,65 +116,11 @@ int tm_to_timet(struct tm *cd)
   return result;
 }
 
-/*
-bool isDst(int local)
-{
-  int mar, nov;
-  struct tm *tmp, tm;
-  int year;
-	
-  tmp = timet_to_tm(local);
-  tmp->tm_mon = 3;
-  tmp->tm_mday = 1;
-  mar = tm_to_timet(tmp);
-  tmp = timet_to_tm(mar);
-  mar = tmp->tm_wday;
-  mar = (7-mar) % 7 + 1;
-  mar += 7;
-  tmp->tm_mday = mar;
-  tmp->tm_hour = 2;
-  tmp->tm_min = 0;
-  mar = tm_to_timet(tmp);
-	
-  tmp = timet_to_tm(local);
-  tmp->tm_mon = 11;
-  tmp->tm_mday = 1;
-  nov = tm_to_timet(tmp);
-  tmp = timet_to_tm(nov);
-  nov = tmp->tm_wday;
-  nov = (7-nov) % 7 + 1;
-  tmp->tm_mday = nov;
-  tmp->tm_hour = 2;
-  tmp->tm_min = 0;
-  nov = tm_to_timet(tmp);
-  
-  return (local >= mar && local <= nov);
-  // plus some sort of flag ... has this happened already, etc
-}
-*/
-
 int gm_time(int ltime)
-{
-  struct tm *tm_dst;
-  int tz = TZ;
-	
-  tm_dst = localtime((time_t*)&ltime);
-  if (tm_dst->tm_isdst) {
-	tz += 1; 
-	}
-	
-  ltime -= tz*60*60;
-	
+{	
+  ltime += offset*60;	
   return ltime;
 }
-
-/*
-int local_time(int utime)
-{
-  return utime + TZ*60*60;
-}
-
-*/
 
 void update_display(int now) 
 {
@@ -195,14 +130,6 @@ void update_display(int now)
   char *dst_text;
   struct tm *tm_local, *tm_utc;
   int gm_now = gm_time(now);
-  struct tm *tm_dst;
-	
-  tm_dst = localtime((time_t*)&now);
-  if (tm_dst->tm_isdst) {
-	dst_text = "DT"; 
-  } else {
-	dst_text = "ST";
-	}
 	
   tm_local = timet_to_tm(now);
   snprintf(time_text, 6, "%.2d:%.2d", tm_local->tm_hour, tm_local->tm_min);
@@ -213,7 +140,7 @@ void update_display(int now)
 		                  tm_local->tm_hour  , tm_local->tm_min, 
 		                  dows[tm_local->tm_wday], 
 		                  tm_local->tm_mday, mons[tm_local->tm_mon-1],
-		                  dst_text);
+		                  tzname);
   tm_utc   = timet_to_tm(gm_now);
 #ifdef SECONDS
 
@@ -230,7 +157,6 @@ void update_display(int now)
   text_layer_set_text(text_date_layer, date_text);
   text_layer_set_text(text_time_layer, time_text);
   text_layer_set_text(text_UTC_layer, utc_text);
-  // text_layer_set_text(text_dst_layer, dst_text);
 }
 
 static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
@@ -238,6 +164,20 @@ static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
   update_display(now);
 }
 
+static void in_received_handler(DictionaryIterator *iter, void *context)
+{
+  Tuple *ofs = dict_find(iter, 0);
+  Tuple *tzn = dict_find(iter, 1);
+	
+  offset = (int)ofs->value->int32;
+  strncpy(tzname, tzn->value->cstring, 4);	
+}
+
+void appmessage_init(void)
+{
+  app_message_register_inbox_received(in_received_handler);
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+}
 
 void init()
 {
@@ -249,8 +189,6 @@ void init()
   Layer *root_layer = window_get_root_layer(window);
   GRect frame = layer_get_frame(root_layer);
 
-	//6, 30, 144-6, 168-30
-  // wday mon mday
   text_date_layer = text_layer_create(GRect(0, 10, frame.size.w, frame.size.h-10));
   text_layer_set_text_color(text_date_layer, GColorWhite);
   text_layer_set_background_color(text_date_layer, GColorClear);
@@ -296,23 +234,6 @@ void init()
   text_layer_set_text(text_conn_layer, "Conn?");
   layer_add_child(root_layer, text_layer_get_layer(text_conn_layer));	
 
-	  // DST
-/*
-  text_dst_layer = text_layer_create(GRect(frame.size.w/2, 0, frame.size.w/2-10, frame.size.h-142));
-  text_layer_set_text_color(text_dst_layer, GColorWhite);
-  text_layer_set_background_color(text_dst_layer, GColorClear);
-  text_layer_set_font(text_dst_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-  text_layer_set_text_alignment(text_dst_layer, GTextAlignmentRight);
-  text_layer_set_text(text_dst_layer, "DST?");
-  layer_add_child(root_layer, text_layer_get_layer(text_dst_layer));	
-*/	
-	// line
-/*  layer_init(&line_layer, window.layer.frame);
-  line_layer.update_proc = &line_layer_update_callback;
-  layer_add_child(&window.layer, &line_layer);
-*/
-  // Avoid blank display on launch
-
   update_display(time(NULL));
 	
 #ifdef SECONDS	
@@ -324,6 +245,7 @@ void init()
   handle_batt(battery_state_service_peek());
   bluetooth_connection_service_subscribe(handle_conn);
   handle_conn(bluetooth_connection_service_peek());
+  appmessage_init();
 }
 
 void deinit(void) 
---
+++ src/pebble-js-app.js
@@ -0,0 +1,40 @@
+function get_tzname() 
+{
+  var d = new Date()
+  var year = d.getFullYear();
+  var jan = new Date(year, 1, 1, 0, 0, 0, 0);
+  var soffs = jan.getTimezoneOffset();
+  var offs = d.getTimezoneOffset();
+  var zname = "";
+
+  if (soffs == 300) {
+    zname = "E";
+  } else if (soffs == 360) {
+    zname = "C";
+  } else if (soffs == 420) {
+    zname = "M";
+  } else if (soffs == 480) {
+    zname = "P";
+    }
+
+  if (offs != soffs) {
+    zname = zname + "DT";
+  } else {
+    zname = zname + "ST"; 
+    }
+
+  return zname;
+}
+
+var d = new Date();
+
+Pebble.addEventListener('ready',
+  function(e)
+  {
+    var transactionId = Pebble.sendAppMessage( 
+	                    { '0': d.getTimezoneOffset(), '1': get_tzname() },
+                        function(e) { console.log('Success'); },
+                        function(e) { console.log('Failure'); }
+	                    );
+  }
+);
